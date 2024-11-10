6756113b2f62e44977bacecedcd13c07c3a76513
VasekCZ230 <122147379+VasekCZ230@users.noreply.github.com>
2024-11-10 12:26:40 +0100

    Add files via upload

---
+++ php/counter/counter.php
@@ -0,0 +1,11 @@
+<?php
+$db="count.txt";
+$handle=fopen($count, 'r+');
+$data=fread($handle, 512);
+$count=$data + 1;
+print $count;
+fseek($handle, 0) ;
+fwrite($handle, $count) ;
+fclose($handle) ;
+
+?>
