127f78d57fda92cc7506040dead7a74247f12d9a
VasekCZ230 <122147379+VasekCZ230@users.noreply.github.com>
2025-08-25 18:37:21 +0200

    Update searchbox.js

--- js/searchbox.js
+++ js/searchbox.js
@@ -11,14 +11,30 @@ function searchSite() {
   var query = document.getElementById("searchBox").value.toLowerCase();
   var resultsDiv = document.getElementById("results");
   resultsDiv.innerHTML = "Hledám...";
-  
+
   var results = [];
 
   Promise.all(pages.map(function(url) {
     return fetch(url).then(function(resp) { return resp.text(); })
-      .then(function(text) {
-        if (text.toLowerCase().indexOf(query) !== -1) {
-          results.push('<li><a href="'+url+'">'+url+'</a></li>');
+      .then(function(html) {
+        // vytáhneme čistý text (odstraníme HTML tagy)
+        var text = html.replace(/<[^>]+>/g, " ").toLowerCase();
+
+        var index = text.indexOf(query);
+        if (index !== -1) {
+          // úryvek kolem výsledku
+          var start = Math.max(0, index - 40);
+          var end = Math.min(text.length, index + query.length + 40);
+          var snippet = text.substring(start, end);
+
+          // zvýraznění hledaného slova
+          var re = new RegExp("(" + query + ")", "gi");
+          snippet = snippet.replace(re, "<mark>$1</mark>");
+
+          results.push(
+            '<li><a href="'+url+'">'+url+'</a><br><small>... '
+            + snippet + ' ...</small></li>'
+          );
         }
       }).catch(function(e) {
         console.log("Nepodařilo se načíst " + url, e);
@@ -31,7 +47,7 @@ function searchSite() {
     }
 
     // odkaz na Google hledání
-    var googleLink = "https://www.google.com/search?q=" + encodeURIComponent(query);
+    var googleLink = "https://www.google.com/search?q=site:username.github.io+" + encodeURIComponent(query);
     resultsDiv.innerHTML += '<p><a href="'+googleLink+'" target="_blank">Hledat na Googlu</a></p>';
   });
 }
