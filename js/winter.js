bd22e3fde8ab11d9ba0dd632251a52205dfde5c1
VasekCZ230 <122147379+VasekCZ230@users.noreply.github.com>
2025-11-19 07:39:42 +0100

    Add winter.js to load snow.js during winter

---
+++ js/winter.js
@@ -0,0 +1,20 @@
+<script>
+var winterStartDay = 1;
+var winterStartMonth = 11; //0 = leden → 11 = prosinec
+
+var winterEndDay = 28; 
+var winterEndMonth = 1;
+
+var now = new Date();
+var year = now.getFullYear();
+
+var startDate = new Date(year, winterStartMonth, winterStartDay);
+var endDate = new Date(year + (winterEndMonth < winterStartMonth ? 1 : 0), winterEndMonth, winterEndDay);
+
+if (now >= startDate && now <= endDate) {
+    var script = document.createElement("script");
+    script.src = "snow.js";
+    script.async = true;
+    document.head.appendChild(script);
+}
+</script>
